06ed04217eaaf9c055a161b1cdd383a430b157ee
Arda Coşkun , 1230505008 <149784249+1230505008@users.noreply.github.com>
2025-02-14 18:33:19 +0300

    Add files via upload

---
+++ TopluTasimaOdev/src/Durak.java
@@ -0,0 +1,20 @@
+public class Durak {
+    String ad;
+    double[] veriler;
+
+    // Constructor
+    public Durak(String ad, double[] veriler) {
+        this.ad = ad;
+        this.veriler = veriler;
+    }
+
+    // Getter metodu
+    public String getAd() {
+        return ad;
+    }
+
+    // Veriler getter metodu
+    public double[] getVeriler() {
+        return veriler;
+    }
+}
